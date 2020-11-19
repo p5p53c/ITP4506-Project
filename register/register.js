230b9fb7596a2c36c1638b3793dbc6a67c9074eb
p5p53c <p5p53c@gmail.com>
2020-11-19 20:19:04 +0800

    Change Agent Sign up from index to manager

--- register/register.js
+++ register/register.js
@@ -2,7 +2,7 @@ function register(theForm) {
     var name = this.inputFirstName.value;
     name += " " + this.inputLastName.value;
     var mail = this.inputEmail.value;
-    alert("Thanks for your register\n" + name + "Please go to your mail box : " + mail + " check the confirm email");
-    alert("Now return to home page");
-    location.href = "../index.html";
+    alert("Thanks for your register\n" + name + ", please go to your mail box : " + mail + " check the confirm email");
+    alert("Go back to previous page");
+    window.history.back();
 }
